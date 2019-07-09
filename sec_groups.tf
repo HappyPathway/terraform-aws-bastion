ca9bd09737a5fa938a44cc6a30e9e217891ba86f
David Arnold <dja+djaboxx@sonic.net>
2019-07-09 14:49:04 -0700

    Update sec_groups.tf

--- sec_groups.tf
+++ sec_groups.tf
@@ -8,7 +8,12 @@ resource "aws_security_group" "bastion" {
     protocol    = "tcp"
     cidr_blocks = ["${var.ssh_access}"]
   }
-  
+  ingress {
+    from_port   = 23
+    to_port     = 23
+    protocol    = "tcp"
+    cidr_blocks = ["${var.ssh_access}"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
